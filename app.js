94771eb4e29c19746adc0342d0ff0ea646a20dbf
Deeumiya Huang <Abyukuri@gmail.com>
2026-05-30 20:41:01 +1200

    feat: setup basic app packages

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 // Load a .env file if one exists
 require('dotenv').config()
 
+// Setup Express
 const express = require("express");
-const handlebars = require("express-handlebars");
 const app = express();
-
 // Listen port will be loaded from .env file, or use 3000
 const port = process.env.EXPRESS_PORT || 3000;
 
 // Setup Handlebars
-app.engine("handlebars", handlebars.create({
+const handlebars = require("express-handlebars");
+app.engine("handlebars", handlebars.engine({
     defaultLayout: null
-}).engine);
+}));
 app.set("view engine", "handlebars");
 
 // Set up to read POSTed form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({}));
 
+// Setup cookie-parser
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
-// TODO: Your app here
+// Setup express-session
+const session = require("express-session");
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+        maxAge: 900000
+    }
+}));
+
+// Make the "public" folder available statically
+const path = require("path");
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 
 app.listen(port, function () {
