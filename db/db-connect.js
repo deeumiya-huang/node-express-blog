591d7c2fe41b2f8fd471bff32ef187f8fe1bda7b
Deeumiya Huang <Abyukuri@gmail.com>
2026-05-31 09:49:19 +1200

    feat: create db connect

---
+++ db/db-connect.js
@@ -0,0 +1,10 @@
+const mariadb = require("mariadb");
+
+const database = mariadb.createConnection({
+    host: process.env.DB_HOST,
+    database: process.env.DB_USER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+});
+
+module.exports = database;
